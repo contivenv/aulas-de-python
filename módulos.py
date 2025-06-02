ef104fea5d9b73cca243e14c65122fd28c5c6f65
Guilherme Conti Teixeira <guilhermecontiteixeira256@gmail.com>
2025-06-02 16:53:15 -0300

    Estudos diários 04

---
+++ módulos.py
@@ -0,0 +1,7 @@
+import math # módulo de funções matemáticas
+from math import sqrt # importar somente a funcionalidade da biblioteca math sobre raiz quadrada
+import match as m # criar um alias para dar um apelido a biblioteca
+
+print(m.sqrt(81)) # precisamos sempre escrever o nome do módulo que queremos + sua funcionalidade. Nesse caso, pedimos para que o programa nos desse a raiz quadrada de 81. O resultado foi 9.0.
+
+import exercicio1 as e

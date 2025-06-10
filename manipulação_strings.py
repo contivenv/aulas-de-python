9eadb23706dd1b9b5d861124dcacb01cab259c9f
Guilherme Conti Teixeira <guilhermecontiteixeira256@gmail.com>
2025-06-10 16:56:47 -0300

    Estudos diários 08

---
+++ manipulação_strings.py
@@ -0,0 +1,28 @@
+# são imutavei, consegue mudar a variável que contem a string
+
+# nome = 'Aluno'
+# aluno = 'Guilherme'
+# # letra = nome[2]
+# # print(letra)
+# # print(len(nome))
+# print(nome + ' é o ' + aluno)
+
+# frase = 'Iremos aprender Python hoje'
+# palavras = frase.split()
+# print(palavras[1])
+# for letra in frase:
+#     print(letra)
+
+# print(frase[0:5])
+
+# email = input('Digite seu endereço de email: ')
+# arroba = email.find('@')
+# print(arroba)
+# usuario = email[0:arroba]
+# dominio = email[arroba+1:]
+# print(usuario)
+# print(dominio)
+
+produtos = 'carbonato de sódio e óxido de zinco'
+print('sódio' in produtos)
+print('magnésio' in produtos)
